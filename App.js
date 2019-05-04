b37ca368ed5908ed4df44e3264042e0f8d6f75d3
Yang <yangah9991@gmail.com>
2019-05-04 23:51:56 +0800

    Setting Up Default Front Page

--- App.js
+++ App.js
@@ -1,16 +1,44 @@
-import React from 'react'
-import { View, Text } from "react-native";
+import React from 'react';
+import { View, Text, Platform, Image, ImageBackground } from "react-native";
+import {Button} from 'native-base';
+
+var landingBackground = require('./assets/icons/landing.jpg');
 
 class App extends React.Component
 {
     render()
     {
         return(
-            <View>
-                
+            <View style = {style.Container}>
+                <ImageBackground  source={landingBackground} style = {{width: '100%', height: '100%'}}>
+                    <View  style = {style.AppWrapper}> 
+                        <Text style = {style.AppTitle}> Welcome To Poke Search</Text>
+                    </View>
+                </ImageBackground >
             </View>
-        )
+        );
     }
+}
+
+const style = 
+{
+    Container : 
+    {
+        flex:1,
+    },
+    AppWrapper :
+    {
+        flex : 1,
+        flexDirection : 'column',
+        alignItems : 'center',
+        justifyContent : 'center',
+        marginTop : Platform.OS == "android" ? 24 : 0,
+    },
+    AppTitle :
+    {
+        fontSize : 30,
+        fontWeight : 'bold',
+    },
 }
 
 export default App;
